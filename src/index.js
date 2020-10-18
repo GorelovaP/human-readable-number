87f1db1736ed5b7d4e7c491ceda2c3469d6eb486
GorelovaP <70329025+GorelovaP@users.noreply.github.com>
2020-10-18 17:25:58 +0300

    Fix: new code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function towelSort (matrix)
+{
+if (!matrix) {
+return [];}
+else
+{
+a=[];
+for(var i = 0; i<matrix.length; i ++)
+{
+if(i%2!==0) {a.push(matrix[i].reverse())}
+else if (matrix[i] === []) {return a}
+else {a.push(matrix[i])}
+}
+return a.flat();
+}
 }
